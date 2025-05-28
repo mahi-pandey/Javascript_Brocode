b639f3ac5c654449c1c8956b9c0c6d31e1780204
mahima <mahipandey2206@gmail.com>
2025-05-28 18:49:51 +0530

    Spread and rest operator

---
+++ temperature conversion/practice/index3.js
@@ -0,0 +1,80 @@
+/*let numbers=[1,2,3,45,5];
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+console.log(minimum);
+console.log(maximum);
+
+
+let username = "Mahima Pandey";
+
+let letters=[...username].join("-  ");
+
+console.log(letters)
+
+let fruits=["apple","banana","mango"];
+let veg=["potato","carrot"];
+
+let food = [...fruits,...veg , "eggs" ,"apl"];
+console.log(food);
+
+
+function openfridge(...foods){
+    console.log(...foods)
+}
+
+
+
+
+const food1='piza';
+const food2='burger';
+const food3='saminata';
+const food4='aqride';
+
+
+openfridge(food1,food2,food3,food4);*/
+
+
+
+/*function sum(...numbers){
+    let result=0;
+    for(let number of numbers){
+        result+=number;
+    }
+
+    return result;
+}
+
+
+
+const total = sum(1,2,2,4,4,44);
+
+console.log(`your total is ${total}`)*/
+
+
+
+/*function getaverage(...numbers){
+    let result=0;
+    for(let number of numbers){
+        result+=number;
+    }
+
+    return result/numbers.length;
+}
+
+
+const total = Math.floor(getaverage(23,56,78,78,78,78));
+
+console.log(`${total}`)*/
+
+
+
+
+
+
+
+
+
+
+
+
